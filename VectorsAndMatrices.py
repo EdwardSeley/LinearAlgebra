6e9fbb7c54c15253435ceac3f0c082ad51ffdfc8
EdwardSeley <edwardseley@gmail.com>
2017-04-05 00:38:49 -0500

    Added matrix multiplication function as well as a function to switch the dimensions of a matrix

--- VectorsAndMatrices.py
+++ VectorsAndMatrices.py
@@ -16,7 +16,7 @@ class Vector:
         for x in range(len(data)):
             if type(data[x]) != int and type(data[x]) != float:
                 raise ValueError("Components must be integers or decimals. Item "
-                                 + str(data[x]) + "is type " + str(type(data[x])))
+                                 + str(data[x]) + " of component number " + str(x) + " is type " + str(type(data[x])))
         self.components = data
         self.dimension = len(data)
 
@@ -70,7 +70,7 @@ class Vector:
         Returns the dot product of both vectors.
         """
         if len(self) != len(other):
-            raise ValueError("Vectors must have the same length")
+            raise ValueError("Vectors must have the same length. Vector 1 has length: " + str(len(self)) + " and Vector two has length: " + str(len(other)))
         dot_sum = 0
         for x in range(0, len(self)):
             dot_sum = dot_sum + self.components[x] * other.components[x]
@@ -114,8 +114,10 @@ class Matrix:
             raise ValueError("Parameter must be a list of a integer or decimal rows")
         for x in range(0, len(data)):
             if type(data[x]) != list:
+                print(str(len(data)))
                 raise ValueError("Parameter must be a list of a integer or decimal rows. "
-                                 "the item number " + str(x) + " in the list is not a list")
+                                 "the item number " + str(x) + " in the list is not a list. It is of type: "
+                                 + str(type(data[x])))
             if len(data[x]) != len(data[0]):
                 raise ValueError("Each row of the matrix must have the same size. " + "Row 1 has size " + str(len(data[0]))
                 + " and row " + str(x + 1) + " has length " + str(len(data[x])))
@@ -125,9 +127,21 @@ class Matrix:
                                      + "The entry in row " + str(x) + "and column " + str(y)
                                      + "is not an integer or decimal")
 
+        columnList = [None] * len(data)
+        vecList = [None] * len(data[0])
+
+        for a in range(0, len(data[0])): #iterates through number of rows
+            for b in range(0, len(data)): #iterates through number of columns
+                #print("a: " + str(a) + " out of " + str(len(data[0]) - 1) + " b: " + str(b) + " out of " + str(len(data) - 1))
+                columnList[b] = data[b][a]
+            vec = Vector(columnList)
+            vecList[a] = vec
+            columnList = [None] * len(data)
+
+        self.columnVecs = vecList
         self.entries = data
-        self.columnSize = len(data)
-        self.rowSize = len(data[0])
+        self.num_of_columns = len(data[0])
+        self.num_of_rows = len(data)
 
     def __add__(self, other):
         """
@@ -135,13 +149,13 @@ class Matrix:
         adds the respective entries together and stores them in a 2D list.
         Returns the matrix of the added entries
         """
-        if self.columnSize != other.columnSize:
+        if self.num_of_columns != other.columnSize:
             raise ValueError("Cannot add matrices with different numbers of columns")
-        if self.rowSize != other.rowSize:
+        if self.num_of_rows != other.rowSize:
             raise ValueError("Cannot add matrices with different numbers of row entries")
-        addition_matrix = [ [0 for a in range(0, self.rowSize)] for b in range(0, self.columnSize) ]
-        for x in range(0, self.columnSize):
-            for y in range(0, self.rowSize):
+        addition_matrix = [ [0 for a in range(0, self.num_of_columns)] for b in range(0, self.num_of_rows) ]
+        for x in range(0, self.num_of_rows):
+            for y in range(0, self.num_of_columns):
                 addition_matrix[x][y] = self.entries[x][y] + other.entries[x][y]
         return Matrix(addition_matrix)
 
@@ -153,9 +167,9 @@ class Matrix:
         """
         if type(scalar) != int and type(scalar) != float:
             raise ValueError("Scalar must be an integer or decimal")
-        scaled_matrix = [[0 for a in range(0, self.rowSize)] for b in range(0, self.columnSize)]
-        for x in range(0, self.columnSize):
-            for y in range(0, self.rowSize):
+        scaled_matrix = [[0 for a in range(0, self.num_of_columns)] for b in range(0, self.num_of_rows)]
+        for x in range(0, self.num_of_rows):
+            for y in range(0, self.num_of_columns):
                 scaled_matrix[x][y] = self.entries[x][y] * scalar
         return Matrix(scaled_matrix)
 
@@ -165,12 +179,46 @@ class Matrix:
         row of the matrix and the vector
         Returns the product of a matrix and vector.
         """
-        if len(vector) != self.rowSize:
+        if len(vector) != self.num_of_columns:
             raise ValueError("Vector size must equal matrix row length")
         temp_list = []
-        for x in range(0, self.columnSize):
+        for x in range(0, self.num_of_rows):
             temp_list.append(Vector(self.entries[x]).dotProduct(vector))
         return Vector(temp_list)
+
+    def matrix_mult(self, other):
+        """
+        Checks whether or not the number of columns in the first matrix matches the number of rows in the second matrix.
+        Loops through the columns of the second matrix and stores the dot product of each column with the first matrix
+        in a list. After the list is complete, it is used to create a matrix and then that matrix's dimensions are
+        changed.
+        """
+        if self.num_of_columns != other.num_of_rows:
+            raise ValueError("Row size of 2nd Matrix must equal column size of first. Matrix 1 has a number of " + str(self.num_of_columns) + " and Matrix 2 has a row number of " + str(other.num_of_rows) )
+
+        horizontal_matrix = [None] * other.num_of_columns
+        for x in range(0, other.num_of_columns):
+            horizontal_matrix[x] = self.vector_mult(other.columnVecs[x]).components
+
+        product_matrix = Matrix(horizontal_matrix).switchDimensions()
+        return product_matrix
+
+    def switchDimensions(self):
+        """
+        Loops through the rows within a loop of the columns (resets rows after it moves to the next column) and finds
+        the corresponding members of the same column and puts them in a list. This list of same column members are then
+        made a row by creating a list of each list of same column entries. The list is then converted into a matrix. 
+        """
+        tempList = [None] * self.num_of_rows
+        rowList = [None] * self.num_of_columns
+
+        for x in range(self.num_of_columns):
+            for y in range(0, self.num_of_rows):
+                tempList[y] = self.entries[y][x]
+            rowList[x] = tempList;
+            tempList = [None] * self.num_of_rows
+
+        return Matrix(rowList)
 
     def print(self):
         """
@@ -179,9 +227,11 @@ class Matrix:
         Returns nothing
         """
         print('\n')
-        for x in range(0, self.columnSize):
-            for y in range(0, self.rowSize):
+        for x in range(0, self.num_of_rows):
+            for y in range(0, self.num_of_columns):
                 print(self.entries[x][y], end=' ')
             print('\n')
 
 
+m1 = Matrix([[3, 7, 1], [6, 9, 3], [6, 2, 4], [5, 2, 8]])
+m1.switchDimensions().print()
