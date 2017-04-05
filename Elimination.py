6330b968d34b00a407d05155cabf6a0768a90299
EdwardSeley <edwardseley@gmail.com>
2017-04-05 13:51:58 -0500

    Function that solves matrix based off solution provide

--- Elimination.py
+++ Elimination.py
@@ -7,22 +7,29 @@ def e21(baseMatrix):
     e21 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0], [-multiplier/pivot, 1, 0], identityMatrix(3).entries[2]])
     return e21
 
+def e21(baseMatrix):
+    pivot = baseMatrix.entries[0][0]
+    multiplier = baseMatrix.entries[1][0]
+    e21 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0], [-multiplier/pivot, 1, 0], identityMatrix(3).entries[2]])
+    return e21
+
 def e31(baseMatrix):
     pivot = baseMatrix.entries[0][0]
     multiplier = baseMatrix.entries[2][0]
-    e31 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0], identityMatrix(baseMatrix.num_of_rows).entries[1],
-                   [-multiplier/pivot, 0, 1]])
+    e31 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0],
+                identityMatrix(baseMatrix.num_of_rows).entries[1],
+                [-multiplier/pivot, 0, 1]])
     return e31
 
 def e32(baseMatrix):
     pivot = baseMatrix.entries[1][1]
     multiplier = baseMatrix.entries[2][1]
-    e32 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0], identityMatrix(baseMatrix.num_of_rows).entries[1],
+    e32 = Matrix([identityMatrix(baseMatrix.num_of_rows).entries[0],
+                   identityMatrix(baseMatrix.num_of_rows).entries[1],
                    [0, -multiplier/pivot, 1]])
     return e32
 
 def identityMatrix(size):
-
     tempList = [0] * size
     rowList = [None] * size
     for x in range(0, size):
@@ -36,6 +43,19 @@ def u_matrix(baseMatrix):
     m2 = e31(m1).matrix_mult(m1)
     return e32(m2).matrix_mult(m2)
 
-m1 = Matrix([[2, 4, -2], [4, 9,-3], [-2, -3, 7]])
-m2 = u_matrix(m1)
-m2.print()
+def solutionAugment(baseMatrix, solution):
+    vec1 = e21(baseMatrix).vector_mult(solution)
+    matrix_one = e21(baseMatrix).matrix_mult(baseMatrix)
+    vec2 = e31(matrix_one).vector_mult(vec1)
+    matrix_two = e31(matrix_one).matrix_mult(matrix_one)
+    solution_vec = e32(matrix_two).vector_mult(vec2)
+    return solution_vec
+
+def solveMatrix(baseMatrix, solution):
+    eliminated_matrix = u_matrix(baseMatrix)
+    eliminated_solution = solutionAugment(baseMatrix, solution)
+    x3 = eliminated_solution.components[2]/eliminated_matrix.entries[2][2]
+    x2 = (eliminated_solution.components[1]  -  eliminated_matrix.entries[1][2] * x3)/eliminated_matrix.entries[1][1]
+    x1 = (eliminated_solution.components[0] - eliminated_matrix.entries[0][1] * x2
+          - eliminated_matrix.entries[0][2]* x3)/eliminated_matrix.entries[0][0]
+    return Vector([x1, x2 ,x3])
