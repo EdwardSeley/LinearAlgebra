63268d6c416b1c6663f26503fcae2c18ea793300
EdwardSeley <edwardseley@gmail.com>
2017-04-09 14:01:11 -0500

    Elimination now works for 2x2 matrices as well. Floating point results are also rounded

--- Elimination.py
+++ Elimination.py
@@ -1,16 +1,16 @@
 from VectorsAndMatrices import Matrix, Vector
-
-
-def e21(baseMatrix):
-    pivot = baseMatrix.entries[0][0]
-    multiplier = baseMatrix.entries[1][0]
-    e21 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0], [-multiplier/pivot, 1, 0], identityMatrix(3).entries[2]])
-    return e21
+from decimal import *
 
 def e21(baseMatrix):
     pivot = baseMatrix.entries[0][0]
     multiplier = baseMatrix.entries[1][0]
-    e21 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0], [-multiplier/pivot, 1, 0], identityMatrix(3).entries[2]])
+    if (baseMatrix.num_of_columns == 3):
+        e21 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0],
+                       [round(-multiplier/pivot, 5), 1, 0],
+                       identityMatrix(3).entries[2]])
+    else:
+        e21 = Matrix([identityMatrix(baseMatrix.num_of_rows).entries[0],
+                      [round(-multiplier / pivot, 5), 1]])
     return e21
 
 def e31(baseMatrix):
@@ -18,7 +18,7 @@ def e31(baseMatrix):
     multiplier = baseMatrix.entries[2][0]
     e31 = Matrix( [identityMatrix(baseMatrix.num_of_rows).entries[0],
                 identityMatrix(baseMatrix.num_of_rows).entries[1],
-                [-multiplier/pivot, 0, 1]])
+                [round(-multiplier/pivot, 5), 0, 1]])
     return e31
 
 def e32(baseMatrix):
@@ -26,7 +26,7 @@ def e32(baseMatrix):
     multiplier = baseMatrix.entries[2][1]
     e32 = Matrix([identityMatrix(baseMatrix.num_of_rows).entries[0],
                    identityMatrix(baseMatrix.num_of_rows).entries[1],
-                   [0, -multiplier/pivot, 1]])
+                   [0, round(-multiplier/pivot, 5), 1]])
     return e32
 
 def identityMatrix(size):
@@ -40,11 +40,15 @@ def identityMatrix(size):
 
 def u_matrix(baseMatrix):
     m1 = e21(baseMatrix).matrix_mult(baseMatrix)
+    if (baseMatrix.num_of_columns == 2):
+        return m1
     m2 = e31(m1).matrix_mult(m1)
     return e32(m2).matrix_mult(m2)
 
 def solutionAugment(baseMatrix, solution):
     vec1 = e21(baseMatrix).vector_mult(solution)
+    if (baseMatrix.num_of_columns == 2):
+        return vec1
     matrix_one = e21(baseMatrix).matrix_mult(baseMatrix)
     vec2 = e31(matrix_one).vector_mult(vec1)
     matrix_two = e31(matrix_one).matrix_mult(matrix_one)
@@ -54,8 +58,18 @@ def solutionAugment(baseMatrix, solution):
 def solveMatrix(baseMatrix, solution):
     eliminated_matrix = u_matrix(baseMatrix)
     eliminated_solution = solutionAugment(baseMatrix, solution)
-    x3 = eliminated_solution.components[2]/eliminated_matrix.entries[2][2]
-    x2 = (eliminated_solution.components[1]  -  eliminated_matrix.entries[1][2] * x3)/eliminated_matrix.entries[1][1]
-    x1 = (eliminated_solution.components[0] - eliminated_matrix.entries[0][1] * x2
-          - eliminated_matrix.entries[0][2]* x3)/eliminated_matrix.entries[0][0]
-    return Vector([x1, x2 ,x3])
+
+    if (baseMatrix.num_of_columns == 2):
+        x2 = eliminated_solution.components[1]/eliminated_matrix.entries[1][1]
+        x1 = (eliminated_solution.components[0] - eliminated_matrix.entries[0][1] * x2)/eliminated_matrix.entries[0][0]
+        return Vector([round(x1, 3), round(x2, 3)])
+    else:
+        x3 = eliminated_solution.components[2]/eliminated_matrix.entries[2][2]
+        x2 = (eliminated_solution.components[1]  -  eliminated_matrix.entries[1][2] * x3)/eliminated_matrix.entries[1][1]
+        x1 = (eliminated_solution.components[0] - eliminated_matrix.entries[0][1] * x2
+            - eliminated_matrix.entries[0][2]* x3)/eliminated_matrix.entries[0][0]
+    return Vector([round(x1, 3), round(x2, 3), round(x3, 3)])
+
+baseMatrix = Matrix([[3, -1], [1, 1]])
+solution = Vector([5, 3])
+solveMatrix(baseMatrix, solution).print()
